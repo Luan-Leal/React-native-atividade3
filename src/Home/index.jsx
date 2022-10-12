fa023c2c6d40dac267cff871895c016b2fa57e2c
Luan <luanleal05@gmail.com>
2022-10-12 19:40:25 -0300

    first commit

---
+++ src/Home/index.jsx
@@ -0,0 +1,26 @@
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default function Home({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text>Home</Text>
+      <Button
+      title='Sobre'
+      onPress={ () => navigation.navigate('Sobre')}
+      />
+      <Button
+      title='Contato'
+      onPress={ () => navigation.navigate('Contato')}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
